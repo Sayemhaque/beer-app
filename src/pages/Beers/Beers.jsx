14951ccb9e6d8ce82ce1aed48a4d5282582dd2c2
sayemhaque <mdsayemofficial1000@gmail.com>
2023-08-23 18:20:31 +0600

    ui changes

--- src/pages/Beers/Beers.jsx
+++ src/pages/Beers/Beers.jsx
@@ -26,6 +26,7 @@ const Beers = () => {
 
     useEffect(() => {
         getBeers()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [page])
 
 
--- src/pages/Beers/Card.jsx
+++ src/pages/Beers/Card.jsx
@@ -11,18 +11,6 @@ const Card = ({ beer }) => {
             <h2 className="text-xl font-semibold mt-2">{beer.name}</h2>
             <p className="text-gray-300 mb-2">{beer.tagline}</p>
             <p className="text-gray-300">{beer.description?.substring(0,100)}...</p>
-            <div className="mt-4">
-                <span className="bg-gray-200 py-1 px-2 rounded-full text-sm text-gray-700">{beer.abv}% ABV</span>
-                <span className="bg-gray-200 py-1 px-2 rounded-full text-sm text-gray-700 ml-2">{beer.ibu} IBU</span>
-            </div> 
-            <div className="mt-4">
-                <h4 className="text-lg  font-semibold">Food Pairing</h4>
-                <ul className="list-disc list-inside text-gray-800">
-                    {beer.food_pairing.map((pairing, index) => (
-                        <li className="text-gray-300" key={index}>{pairing}</li>
-                    ))}
-                </ul>
-            </div>
         </div>
 
         </Link>
