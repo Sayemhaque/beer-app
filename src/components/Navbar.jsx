11f4ce7bc0e8238fdbc43bd4196e739a16d6288d
sayemhaque <mdsayemofficial1000@gmail.com>
2023-08-25 00:40:10 +0600

    font changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,24 +4,26 @@ import { useAuth } from "../hooks/useAuth";
 
 const Navbar = () => {
     const navigate = useNavigate()
-    const {LogOut} = useAuth()
+    const { LogOut } = useAuth()
     const user = localStorage.getItem("userData")
     const userData = JSON.parse(user)
+
     const handleLogOut = () => {
         LogOut()
         navigate("/")
     }
+
     return (
         <div className="bg-gradient-to-br from-blue-600 to-purple-700 shadow-xl py-4 sticky top-0 z-10 ">
             <div className="flex justify-between items-center md:max-w-6xl mx-auto px-6">
                 {/* logo */}
                 <div>
-                    <h2 className="text-5xl font-bold text-white">BeerHub</h2>
+                    <h2 className="text-3xl md:text-5xl font-serif font-bold text-white">BeerHub</h2>
                 </div>
                 {/* userinfo & logOut button */}
                 <div className="flex justify-center items-center gap-4">
                     <div>
-                        <p className="font-bold text-white">{userData.email}</p>
+                        <p className="hidden md:inline font-bold text-white">{userData.email}</p>
                     </div>
                     <Button
                         title="LogOut"
--- src/pages/Beers/Card.jsx
+++ src/pages/Beers/Card.jsx
@@ -10,8 +10,8 @@ const Card = ({ beer }) => {
         <div  className="h-full w-full bg-gradient-to-b from-blue-400 to-purple-700 text-white rounded-lg  p-4 shadow-lg mx-auto transition-transform transform hover:scale-105">
             <img className="w-12 mx-auto h-60 rounded-md" src={beer.image_url} alt="Beer" />
             <h2 className="text-xl font-semibold mt-2">{beer.name}</h2>
-            <p className="text-gray-300 mb-2">{beer.tagline}</p>
-            <p className="text-gray-300">{beer.description?.substring(0,100)}...</p>
+            <p className="text-md text-gray-300 mt-3 font-sans font-bold">{beer.tagline}</p>
+            <p className="text-gray-300 text-sm mt-3 font-semibold">{beer.description?.substring(0,100)}...</p>
         </div>
 
         </Link>
